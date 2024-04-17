981782af339a85b2b0ea6554b93daf64745b4fc5
Gabriel Perdue <gnperdue@hey.com>
2024-04-17 11:51:53 -0500

    add an option to pass a gate to QRAM; some PEP-8; some work on tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -5,7 +5,9 @@ Usage:
 import unittest
 import sys
 import logging
-from qknn.functions import index_positions
+import qiskit.circuit
+from qiskit.circuit.library import CRYGate, CRZGate
+from qknn.functions import index_positions, diffuser, qram
 
 LOGGER = logging.getLogger(__name__)
 
@@ -19,6 +21,28 @@ class TestFunctions(unittest.TestCase): # pylint: disable=too-many-instance-attr
         self.assertTrue([2], index_positions(4))
         self.assertTrue([1, 4], index_positions(18))
         self.assertTrue([1, 2, 4], index_positions(22))
+
+    def test_diffuser(self):
+        """Test `diffuser()`"""
+        nqubits = 2
+        gate = diffuser(nqubits=nqubits)
+        self.assertEqual(nqubits, gate.num_qubits)
+        self.assertEqual("$U_s$", gate.name)
+        # evidently `to_matrix` is designed to just raise exceptions, which is too bad.
+        with self.assertRaises(qiskit.circuit.exceptions.CircuitError):
+            gate.to_matrix()
+
+    def test_qram(self):
+        """Test `qram()`"""
+        # TODO: test CRY, CRZ, what the circuit does...
+        empty_qram = qram(size_QRAM=1, features=0)
+        self.assertEqual(type(empty_qram), qiskit.circuit.quantumcircuit.QuantumCircuit)
+        empty_crz_qram = qram(size_QRAM=1, features=0, train_set=[], controlled_rotation=CRZGate)
+        self.assertEqual(type(empty_crz_qram), qiskit.circuit.quantumcircuit.QuantumCircuit)
+
+    def test_oracle_st(self):
+        """Test `oracle_st()`"""
+        pass
 
 
 if __name__ == '__main__':
