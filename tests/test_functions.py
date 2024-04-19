87aacbe34ea4c0f08264092730f0d3b8aa523669
Alberto Maldonado <amaldonador1300@alumno.ipn.mx>
2024-04-19 16:57:03 -0600

    add testing

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -6,7 +6,7 @@ import unittest
 import sys
 import logging
 import qiskit
-from qknn.functions import index_positions, diffuser, qram, oracle_st, qknn
+from qknn.functions import index_positions, diffuser, qram, oracle_st, qknn,qknn_experiments
 
 LOGGER = logging.getLogger(__name__)
 
@@ -36,13 +36,32 @@ class TestFunctions(unittest.TestCase): # pylint: disable=too-many-instance-attr
     def test_oracle_st(self):
         """Test `oracle_st()`"""
         LOGGER.info(sys._getframe().f_code.co_name) # pylint: disable=protected-access
-        self.assertIs(qiskit.circuit.quantumcircuit.QuantumCircuit, oracle_st())
+        self.assertIs(qiskit.circuit.quantumcircuit.QuantumCircuit, type(oracle_st()))
 
     def test_qknn(self):
         """Test `qknn()`"""
         LOGGER.info(sys._getframe().f_code.co_name) # pylint: disable=protected-access
         self.assertIs(qiskit.circuit.quantumcircuit.QuantumCircuit, type(qknn()))
 
+
+
+    def test_mae(self):
+        """Test `mae()`"""
+        LOGGER.info(sys._getframe().f_code.co_name) # pylint: disable=protected-access
+        self.assertIs(tuple, type(qknn_experiments().mae_acc()))
+        
+        
+    def test_draw_qknn(self):
+        """Test `draw_qknn()`"""
+        LOGGER.info(sys._getframe().f_code.co_name) # pylint: disable=protected-access
+        self.assertIs(qiskit.circuit.quantumcircuit.QuantumCircuit, type(qknn_experiments().draw_qknn()))
+        
+        
+    def test_print_results(self):
+        """Test `print_results()`"""
+        LOGGER.info(sys._getframe().f_code.co_name) # pylint: disable=protected-access
+        self.assertIs(None, qknn_experiments().print_results())
+        
 
 if __name__ == '__main__':
     unittest.main()
